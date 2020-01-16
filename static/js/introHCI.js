95d93d61baa0420885c6f4869db56a49286c4a88
zachanT <zacharyTchan@gmail.com>
2020-01-16 10:34:59 -0800

    added Javascript!

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,5 +5,14 @@ $(document).ready(function() {
 function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
-            });
+    });
+    $("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+   // prevent the page from reloading
+   e.preventDefault();
+   // In an event handler, $(this) refers to
+   // the object that triggered the event
+   $(this).css("background-color", "#7fff00");
 }
